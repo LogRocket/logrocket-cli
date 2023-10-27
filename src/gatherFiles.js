9b45690173b3e84a27ca7758f5e1e1164c4ce0f0
McKenzieW <weltermckenzie@gmail.com>
2023-10-27 15:02:22 -0400

    don't need recursive getFiles anymore

--- src/gatherFiles.js
+++ src/gatherFiles.js
@@ -3,51 +3,32 @@ import { cwd } from 'process';
 import glob from 'glob';
 import { statSync } from 'fs';
 
-function getFilesRecursively(path, {
-  globString = '**/*.{js,jsx,js.map}',
-  map,
-  realPaths,
-  workingDirectory = cwd(),
-} = {}) {
-  const fullPath = join(workingDirectory, path);
-  if (statSync(fullPath).isFile()) {
-    if (!realPaths.has(fullPath)) {
-      map.push({
-        path: fullPath,
-        name: basename(fullPath),
-      });
-      realPaths.add(fullPath);
-    }
-  } else {
-    try {
-      glob(globString, { cwd: fullPath, sync: true }).forEach(file =>
-        getFilesRecursively(file, {
-          globString,
-          map,
-          realPaths,
-          workingDirectory: fullPath,
-        })
-      );
-    } catch (e) {
-      console.error(e);
-    }
-  }
-}
-
-export async function gatherFiles(paths, {
-  globString = '**/*.{js,jsx,js.map}',
-} = {}) {
+export async function gatherFiles(paths, { globString = '**/*.{js,jsx,js.map}' } = {}) {
   const map = [];
-  const realPaths = new Set();
 
-  await Promise.all(paths.map(path => {
-    return new Promise(resolve => {
-      getFilesRecursively(path, {
-        globString,
-        map,
-        realPaths,
+  await Promise.all(paths.map((path) => {
+    const realPath = join(cwd(), path);
+
+    if (statSync(realPath).isFile()) {
+      map.push({
+        path: realPath,
+        name: basename(realPath),
       });
-      resolve();
+
+      return Promise.resolve();
+    }
+
+    return new Promise(resolve => {
+      glob(globString, { cwd: realPath }, async (err, files) => {
+        for (const file of files) {
+          map.push({
+            path: join(realPath, file),
+            name: file,
+          });
+        }
+
+        resolve();
+      });
     });
   }));
 
